7d84ee84c5639be637979a46fa8a50d33176696a
Ngangbam Indira <ngangbamindiradevi2008@gmail.com>
2025-11-03 00:15:40 +0530

    Update signIn form

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import "./Style.css"
+import fbIcon from "../assets/facebook.png"
+import githubIcon from "../assets/github.png"
+import igIcon from "../assets/instagram.png"
+import linkedinIcon from "../assets/linkedin.png"
+
 
 
 const Form = () => {
 
-const [heading,setHeading] =useState("Sign-In");
-const [btnChange,setChange] =useState("SignIn")
+const [heading,setHeading] =useState("SignIn");
+const [btnChange,setChange] =useState("Sign-In");
   return (
     <div className="container">
         <form action="">
@@ -18,14 +23,19 @@ const [btnChange,setChange] =useState("SignIn")
         </form>
         <div className="side">
           <p>
-            {btnChange==="Sign-In"?"Welcome back! Sign In to access your personalized dashboard.":"Sign Up and take control of your future."}
+            {btnChange==="Sign-In"& heading==="SignIn"?"Welcome back! Sign In to access your personalized dashboard.":"Sign Up and take control of your future."}
           </p>  
             <div className="btn">
                 <button onClick={()=>{setHeading("SignIn");setChange("Sign-In")}}>Sign-In</button>
                 <button onClick={()=>{setHeading("Create new account");setChange("Sign-Up")}}>Sign-Up</button>
             </div>
             
-            
+            <div className="social-media">
+              <img src={fbIcon} />
+              <img src={githubIcon} />
+              <img src={igIcon} />
+              <img src={linkedinIcon} />
+            </div>
         </div>   
     </div>
   )
