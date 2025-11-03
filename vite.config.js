7077b604d6836d89b37fd2eae48a441e4b75e9b8
Ngangbam Indira <ngangbamindiradevi2008@gmail.com>
2025-11-03 13:27:50 +0530

    A react form

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base:  '/signin-form/',
-=======
-  base: '/signin-form/',
->>>>>>> f90830d61e64a72043c0f2129b520dba8ced8139
+
+
+
 })
